b6d84fdb6440a3b29d34a4c46b8b4b8e1211836d
Tom Belmans <r0930357@student.thomasmore.be>
2023-09-18 21:28:37 +0200

    Initiatie tot React

---
+++ Les 1 - Componenten/Initiatie/src/main.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+)
+
+root.render(
+    <>
+        <h1>Hello World!</h1>
+        <h1>Hello Universe!</h1>
+    </>
+)
+
