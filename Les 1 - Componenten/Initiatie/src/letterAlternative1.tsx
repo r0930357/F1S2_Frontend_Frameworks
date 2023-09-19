0dc1505a4051a3428fcb978dda36db742a0ff60d
Tom Belmans <r0930357@student.thomasmore.be>
2023-09-19 19:12:18 +0200

    initiatie

---
+++ Les 1 - Componenten/Initiatie/src/letterAlternative1.tsx
@@ -0,0 +1,14 @@
+import {FunctionComponent} from 'react'
+
+interface LetterAlternative1.tsxProps {
+
+}
+
+const LetterAlternative1.tsx: FunctionComponent<LetterAlternative1.tsxProps> = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default LetterAlternative1.tsx
---
+++ Les 1 - Componenten/Initiatie/src/models/IComputerScientist.tsx
@@ -0,0 +1,9 @@
+interface IComputerScientist {
+    firstName: string
+    lastName: string
+    birth: number
+    death: number
+    accomplishments: string
+}
+
+export default IComputerScientist
